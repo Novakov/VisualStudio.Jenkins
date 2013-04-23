5d2ba6c740ec7ed1e03d5d3cd2e9e1e6d56351f7
Maciej Nowak <maciejt.nowak@gmail.com>
2013-04-23 18:54:40 +0200

    Initial commit

---
+++ JenkinsBuilds/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace MaciejNowak.JenkinsBuilds
+{
+    static class GuidList
+    {
+        public const string guidJenkinsBuildsPkgString = "9408590f-6811-4ff2-9580-e0d89ecfc9f7";
+        public const string guidJenkinsBuildsCmdSetString = "64d5ac09-6076-428d-9d3d-f90900649c53";
+
+        public static readonly Guid guidJenkinsBuildsCmdSet = new Guid(guidJenkinsBuildsCmdSetString);
+    };
+}
