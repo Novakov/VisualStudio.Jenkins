74cf29fb01c942c2cba67a38574da06498a34b41
Maciej Nowak <maciejt.nowak@gmail.com>
2013-05-24 13:36:31 +0200

    Psake build script

---
+++ build.ps1
@@ -0,0 +1,19 @@
+﻿function Invoke-Build 
+{
+    param($Solution, $Target, $Configuration, $LogFile)
+
+    $logger = ''
+
+    if($LogFile -ne $null)
+    {
+        $logger = "/l:FileLogger,Microsoft.Build.Engine;logfile=$($LogFile)"
+    }
+
+    Exec { msbuild $Solution /t:$Target /p:Configuration=$Configuration $logger /v:minimal }
+}
+
+Task Default  -depends Build
+
+Task Build {
+   Invoke-Build -Solution .\JenkinsBuilds.sln -Target Rebuild -Configuration Release -LogFile BuildLog.log
+}
